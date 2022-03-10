9050ae5120047cd63f78f3126123cf585a203dec
TrigonaMinima <shivam_r@outlook.com>
2022-03-11 04:02:26 +0530

    Add: new post - Toastmasters Speech with a Purpose

--- js/heatmap.js
+++ js/heatmap.js
@@ -46,6 +46,7 @@ var data = [
     { "year": years.length - 7 - 1, "month": 7, "value": 2 },
     { "year": years.length - 7 - 1, "month": 9, "value": 1 },
     { "year": years.length - 8 - 1, "month": 1, "value": 1 },
+    { "year": years.length - 8 - 1, "month": 2, "value": 1 },
 ];
 
 
